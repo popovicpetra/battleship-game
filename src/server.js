a051d03709022780c3fec15d207b738cb150d96c
Petra Popovic <ppopovic0411@gmail.com>
2024-09-13 15:48:59 +0200

    hosting 2

--- src/server.js
+++ src/server.js
@@ -4,6 +4,10 @@ const http = require('http');
 const { Server } = require('socket.io');
 const path = require('path');
 
+const dotenv = require('dotenv');
+dotenv.config();
+const port = process.env.PORT;
+
 //Database
 const mongoose = require('mongoose');
 const cors = require('cors');
@@ -29,8 +33,8 @@ const io = new Server(server, {
   },
 });
 
-server.listen(5000, () => {
-  console.log('Server listening on port 5000');
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
 });
 
 const rooms = {};
